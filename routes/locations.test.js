72ad7986aad9ef7c17603d08d0f9be39765a4cb5
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-08 10:18:03 +0100

    Added tests

--- routes/locations.test.js
+++ routes/locations.test.js
@@ -16,54 +16,6 @@ const expectedLocation = {
     "amenities": expect.any(Array)
 }
 
-const testLocation = {
-    location_name: "Test Name 123 !'` ",
-    street: "Test Street 123 !'` ",
-    town: "Test Town 123 !'` ",
-    region: "Test Region 123 !'` ",
-    postcode: "Test Postcode 123 !'` ",
-    location_description: "Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` Test Postcode 123 !'` ",
-    category_seaside: false,
-    category_castles: false,
-    category_caves: false,
-    category_peaceful: true,
-    category_hiking: true,
-    category_hills: true,
-    category_historic: false,
-    category_secluded: false,
-    category_casual: true,
-    category_lakes: true,
-    category_busy: false,
-    category_woods: true,
-    amenities_parking: true,
-    amenities_food: true,
-    amenities_family: true,
-    amenities_changing_facilities: true,
-    amenities_disability_access: true,
-    amenities_peaceful: true,
-    amenities_electric_charging: true,
-    amenities_no_restaurants: false,
-    amenities_museums: false,
-    amenities_beach: false,
-    amenities_hiking: true,
-    amenities_pet_friendly: true,
-    amenities_forests: true,
-    amenities_lots_of_wildlife: true,
-    amenities_watersports: true,
-    amenities_shopping: true,
-    amenities_bodies_of_water: true,
-    amenities_camping: false,
-    amenities_mountains: false,
-    amenities_hearing_loop: false,
-    amenities_public_transport_good: true,
-    amenities_public_transport_bad: false,
-    amenities_accommodation: true,
-    amenities_wifi: true,
-    image_url: "https://media-cdn.tripadvisor.com/media/photo-s/0f/36/11/1a/photo3jpg.jpg",
-    latitude: 51.235323,
-    longitude: -2.2483279
-};
-
 describe("GET /locations", () => {
     test(`
         Sends a GET request to /locations with no parameters
@@ -113,18 +65,6 @@ describe("GET /locations/:id", () => {
             const response = await request(app).get("/locations/a").expect(400);
             expect(response.body).toStrictEqual({ message: expect.any(String) });
             expect(response.body.message).toBe("Endpoint must be an integer.");
-        }
-    );
-})
-
-describe("POST /locations", () => {
-    test(`
-        Sends a POST request to /locations where body = location (i.e. body: {location_id: 1, ... })
-        Checks if the response's HTTP status code is 201
-        Checks if the response's body is an object with the structure: { success: true, payload: [{ location }] }
-        Checks if the location object has the structure expectedLocation
-        `, async () => {
-            const response = await request(app).post("/locations").send(testLocation).expect(201);
         }
     );
 })
