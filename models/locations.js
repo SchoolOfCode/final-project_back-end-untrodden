390543d57a60297b0d41a697a05a61c5b993e4c0
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-09 10:36:15 +0100

    Added PUT route

--- models/locations.js
+++ models/locations.js
@@ -202,3 +202,59 @@ export async function postNewLocation(newLocation) {
 export async function deleteLocationById(id) {
   const response = await db.query(`DELETE FROM locations WHERE location_id = $1`, [id]);
 }
+
+export async function putLocationById(locationId, updatedLocation) {
+  let sqlString = `UPDATE locations SET
+    location_name=$1,
+    street=$2,
+    town=$3,
+    region=$4,
+    postcode=$5,
+    location_description=$6,
+    category_seaside=${updatedLocation.category_seaside},
+    category_castles=${updatedLocation.category_castles},
+    category_caves=${updatedLocation.category_caves},
+    category_peaceful=${updatedLocation.category_peaceful},
+    category_hiking=${updatedLocation.category_hiking},
+    category_hills=${updatedLocation.category_hills},
+    category_historic=${updatedLocation.category_historic},
+    category_secluded=${updatedLocation.category_secluded},
+    category_casual=${updatedLocation.category_casual},
+    category_lakes=${updatedLocation.category_lakes},
+    category_busy=${updatedLocation.category_busy},
+    category_woods=${updatedLocation.category_woods},
+    amenities_parking=${updatedLocation.amenities_parking},
+    amenities_food=${updatedLocation.amenities_food},
+    amenities_family=${updatedLocation.amenities_family},
+    amenities_changing_facilities=${updatedLocation.amenities_changing_facilities},
+    amenities_disability_access=${updatedLocation.amenities_disability_access},
+    amenities_peaceful=${updatedLocation.amenities_peaceful},
+    amenities_electric_charging=${updatedLocation.amenities_electric_charging},
+    amenities_no_restaurants=${updatedLocation.amenities_no_restaurants},
+    amenities_museums=${updatedLocation.amenities_museums},
+    amenities_beach=${updatedLocation.amenities_beach},
+    amenities_hiking=${updatedLocation.amenities_hiking},
+    amenities_pet_friendly=${updatedLocation.amenities_pet_friendly},
+    amenities_forests=${updatedLocation.amenities_forests},
+    amenities_lots_of_wildlife=${updatedLocation.amenities_lots_of_wildlife},
+    amenities_watersports=${updatedLocation.amenities_watersports},
+    amenities_shopping=${updatedLocation.amenities_shopping},
+    amenities_bodies_of_water=${updatedLocation.amenities_bodies_of_water},
+    amenities_camping=${updatedLocation.amenities_camping},
+    amenities_mountains=${updatedLocation.amenities_mountains},
+    amenities_hearing_loop=${updatedLocation.amenities_hearing_loop},
+    amenities_public_transport_good=${updatedLocation.amenities_public_transport_good},
+    amenities_public_transport_bad=${updatedLocation.amenities_public_transport_bad},
+    amenities_accommodation=${updatedLocation.amenities_accommodation},
+    amenities_wifi=${updatedLocation.amenities_wifi},
+    image_url=$7,
+    latitude=${updatedLocation.latitude},
+    longitude=${updatedLocation.longitude}
+    WHERE location_id=${locationId}
+    RETURNING *;`;
+
+  console.log(sqlString);
+  
+  const response = await db.query(sqlString, [updatedLocation.location_name, updatedLocation.street, updatedLocation.town, updatedLocation.region, updatedLocation.postcode, updatedLocation.location_description, updatedLocation.image_url]);
+  return response.rows;
+}
--- routes/locations.js
+++ routes/locations.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllLocationsOnlyTrue, getAllLocations, getLocationById, postNewLocation, deleteLocationById } from "../models/locations.js";
+import { getAllLocationsOnlyTrue, getAllLocations, getLocationById, postNewLocation, deleteLocationById, putLocationById } from "../models/locations.js";
 
 const router = express.Router();
 
@@ -32,7 +32,60 @@ router.post("/", async function(req, res, next) {
   res.status(201).json({ success: true, payload: data});
 })
 
-// router.patch("/:id")
+router.put("/:id", async function (req, res) {
+  let locationId = Number(req.params.id);
+  const updatedLocation = req.body; // 'body' will be the location object, e.g. body.latitude
+  let data = await putLocationById(locationId, testLocationForPut);
+  res.send('Updated Successsfully');
+});
+
+const testLocationForPut = {
+  location_name: 'Test',
+  street: 'Test',
+  town: 'Test',
+  region:'Test',
+  postcode:'Test',
+  location_description:'Test',
+  category_seaside:true,
+  category_castles:true,
+  category_caves:true,
+  category_peaceful:true,
+  category_hiking:true,
+  category_hills:true,
+  category_historic:true,
+  category_secluded:true,
+  category_casual:true,
+  category_lakes:true,
+  category_busy:true,
+  category_woods:true,
+  amenities_parking:true,
+  amenities_food:true,
+  amenities_family:true,
+  amenities_changing_facilities:true,
+  amenities_disability_access:true,
+  amenities_peaceful:true,
+  amenities_electric_charging:true,
+  amenities_no_restaurants:true,
+  amenities_museums:true,
+  amenities_beach:true,
+  amenities_hiking:true,
+  amenities_pet_friendly:true,
+  amenities_forests:true,
+  amenities_lots_of_wildlife:true,
+  amenities_watersports:true,
+  amenities_shopping:true,
+  amenities_bodies_of_water:true,
+  amenities_camping:true,
+  amenities_mountains:true,
+  amenities_hearing_loop:true,
+  amenities_public_transport_good:true,
+  amenities_public_transport_bad:true,
+  amenities_accommodation:true,
+  amenities_wifi:true,
+  image_url:"https://res.cloudinary.com/dnshrtqmv/image/upload/v1659972687/no-image-placeholder_copy_yriogz.png",
+  latitude:50,
+  longitude:2
+}
 
 router.delete('/:id', async function (req, res) {
   let locationId = req.params.id;
